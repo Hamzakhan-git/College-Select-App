a04cc5acc036bb6a96a43fadf97f3d4213179960
Hamzakhan-git <hamzakhan91827@gmail.com>
2026-05-06 08:59:33 +0530

    Connect Backend with Frontend

--- Backened/index.js
+++ Backened/index.js
@@ -6,7 +6,7 @@ const { Pool } = require('pg');
 
 const app = express();
 app.use(cors({
-  origin: ['https://college-select-app-88g1.vercel.app/', 'http://localhost:3000'],
+  origin: ['https://college-select-app-jhcg.vercel.app/', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
